6e410456aa6cca5c7a7658fe385f1fdd63942f41
SHESHU45 <lcb2022056@iiitl.ac.in>
2024-12-15 18:35:59 +0530

    changes

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const ConnectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect('');
+    const MONGODB_URI = process.env.MONGODB_URI as string;
+    await mongoose.connect(MONGODB_URI);
     console.log("DB Connected");
   } catch (error) {
     console.error("DB Connection Error:", error);
